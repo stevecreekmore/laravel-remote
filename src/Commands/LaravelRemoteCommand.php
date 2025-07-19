4c60c6ad50306ac627705ae571ed03164bbcd8d2
Steven Creekmore <creekmore108@gmail.com>
2025-07-19 08:57:21 -0500

    fixed command reserved word issue

--- src/Commands/LaravelRemoteCommand.php
+++ src/Commands/LaravelRemoteCommand.php
@@ -7,13 +7,12 @@ use Spatie\Ssh\Ssh;
 
 class LaravelRemoteCommand extends Command
 {
-    public $signature = 'remote {command}';
+    public $signature = 'remote {myCommand}';
 
     public $description = 'Execute Commands on a remote server';
 
-    public function handle(): void
+    public function handle()
     {
-        dd('executing');
         ssh::create('user', 'example.com')
             ->onOutput(function ($type, $line) {
                 echo $line;
@@ -22,14 +21,6 @@ class LaravelRemoteCommand extends Command
 
     private function getCommandToExecute(): string
     {
-        $command = $this->argument('command');
-
-        if (empty($command)) {
-            $this->error('No command provided.');
-
-            return '';
-        }
-
-        return $command;
+        return  $this->argument('myCommand');
     }
 }
